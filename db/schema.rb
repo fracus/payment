0f1726f7ee61971a16cda07449f021bc5fb36b45
fracus <lamontanarofrancisco@otmail.com>
2017-01-12 19:29:12 -0300

    Agregando Devise al proyecto

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111140838) do
+ActiveRecord::Schema.define(version: 20170112220836) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -29,6 +29,24 @@ ActiveRecord::Schema.define(version: 20170111140838) do
     t.datetime "updated_at"
   end
 
+  create_table "installs", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "installs", ["email"], name: "index_installs_on_email", unique: true
+  add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+
   create_table "time_records", force: true do |t|
     t.date     "date"
     t.float    "total_hs"
@@ -41,5 +59,23 @@ ActiveRecord::Schema.define(version: 20170111140838) do
 
   add_index "time_records", ["category_id"], name: "index_time_records_on_category_id"
   add_index "time_records", ["employee_id"], name: "index_time_records_on_employee_id"
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
